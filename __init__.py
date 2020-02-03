99c29091ae2aab5e79921580d2a8c21df8c6f2fe
Raymond Nijssen <r.nijssen@terglobo.nl>
2020-02-03 22:45:36 +0100

    set GPL v2 to v3

--- __init__.py
+++ __init__.py
@@ -2,11 +2,11 @@
 #-----------------------------------------------------------
 # Copyright (C) 2020 Raymond Nijssen
 #-----------------------------------------------------------
-# Licensed under the terms of GNU GPL 2
+# Licensed under the terms of GNU GPL 3
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #---------------------------------------------------------------------
 
